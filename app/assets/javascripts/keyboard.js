af5e17d8b5e0205e48523d9e66a368625402e387
Fede <fjgimenez@miuandes.cl>
2019-05-08 19:04:29 -0400

    toggle button, constructor and event handler

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,7 +1,38 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+	class KeyboardController {
+		constructor(id_kcontainer,id_targetelement){
+			this.kcontainer = $(id_kcontainer);
+			this.targetelement = $(id_targetelement);
+		}
+
+		KeyPressed(key){
+			key = key.toLowerCase()
+			console.log(key)
+			var te = document.getElementById("targetelement")
+		}
+
+		ToggleVisibility(){
+			var x = document.getElementById("kcontainer");
+			if (x.style.display === "none"){
+				x.style.display = "block";
+			}
+			else{
+				x.style.display = "none";
+			}
+		}
+	}
+
+	var keyboardc = new KeyboardController("kcontainer","targetelement")
+	var toggleButton = document.getElementById("toggleButton")
+
 	$(".key").click(function(){
          console.log(this.innerHTML)
+				 keyboardc.KeyPressed($(this).text());
      });
+
+ toggleButton.addEventListener('click',function(){
+   	keyboardc.ToggleVisibility();
+   },false);
  });
